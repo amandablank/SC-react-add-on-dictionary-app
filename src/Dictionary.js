499b741115a0af93d53a38433134684377d4e6ac
Amanda <amanda.blankenburg@gmail.com>
2023-04-18 17:15:32 +0200

    Installed axios, added the API url and key, created the function handleResponse

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+
+    let apiUrl =
+      "https://api.shecodes.io/dictionary/v1/define?word=serendipity&key=f5a9f38100065t0934bo1b43d42ba03a";
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
@@ -20,3 +29,6 @@ export default function Dictionary() {
     </div>
   );
 }
+
+//https://api.shecodes.io/dictionary/v1/define?word=sunset&key=f5a9f38100065t0934bo1b43d42ba03a
+//apiKey = f5a9f38100065t0934bo1b43d42ba03a
