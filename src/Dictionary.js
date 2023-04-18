9e9a60fd7de0bddb0558599d753a19c36dda01d1
Amanda <amanda.blankenburg@gmail.com>
2023-04-18 17:51:58 +0200

    Added alert to keep the keyword as a used variable

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,6 +11,7 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
 
     let apiUrl =
       "https://api.shecodes.io/dictionary/v1/define?word=serendipity&key=f5a9f38100065t0934bo1b43d42ba03a";
