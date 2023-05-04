e82e0a20406a5c38b7a66329f1e3abf69e579a0f
Amanda <amanda.blankenburg@gmail.com>
2023-05-04 12:25:11 +0200

    Added the images API, created a Photo component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,22 +2,32 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 import icon from "./images/search.png";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data);
+  }
+
+  function handleImagesResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
     event.preventDefault();
 
-    let apiKey = "f5a9f38100065t0934bo1b43d42ba03a";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    let dictionaryApiKey = "f5a9f38100065t0934bo1b43d42ba03a";
+    let dictionaryApiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${dictionaryApiKey}`;
+    axios.get(dictionaryApiUrl).then(handleDictionaryResponse);
+
+    let imagesApiKey = "f5a9f38100065t0934bo1b43d42ba03a";
+    let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imagesApiKey}`;
+    axios.get(imagesApiUrl).then(handleImagesResponse);
   }
 
   function handleKeywordChange(event) {
@@ -45,6 +55,7 @@ export default function Dictionary() {
         </div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
